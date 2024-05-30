5e5c49ab5ae5a958e13f456742adfd715986e7c7
naSim087 <nasimsheikh688@gmail.com>
2024-05-30 17:33:17 +0530

    first commit for the booking service

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express= require('express')
+const {PORT}=require('./config/server-config')
+const app= express();
+const bodyParser=require('body-parser')
+const apiRoutes=require('./routes/index')
+const db=require('./models/index');
+const setUpAndStartServer=()=>{
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended:true}));
+  app.use('/api',apiRoutes);
+  app.listen(PORT,()=>{
+    console.log(`Server Started at ${PORT}`)
+    if(process.env.DB_SYNC){
+      db.sequelize.sync({alter:true});
+    }
+  })
+}
+setUpAndStartServer()
